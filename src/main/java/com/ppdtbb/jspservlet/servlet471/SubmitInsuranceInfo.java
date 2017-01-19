baef4b07b9924dbe4f3d05e2f818bb5afbc524b1
wanggang@enaea.edu.cn <wanggang@enaea.edu.cn>
2017-01-19 17:46:44 +0800

    添加下载方法

--- src/main/java/com/ppdtbb/jspservlet/servlet471/SubmitInsuranceInfo.java
+++ src/main/java/com/ppdtbb/jspservlet/servlet471/SubmitInsuranceInfo.java
@@ -1,7 +1,12 @@
 package com.ppdtbb.jspservlet.servlet471;
 
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 
 public class SubmitInsuranceInfo extends HttpServlet {
-
+	
+	public void download(HttpServletRequest request) {
+		
+	}
+	
 }

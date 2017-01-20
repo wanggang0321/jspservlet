7484447c2f606cad401d19487f3aecba98e786f5
wanggang@enaea.edu.cn <wanggang@enaea.edu.cn>
2017-01-20 10:08:07 +0800

    提交

--- src/main/java/com/ppdtbb/seven/DownloadTest.java
+++ src/main/java/com/ppdtbb/seven/DownloadTest.java
@@ -4,7 +4,6 @@ import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.URLEncoder;
@@ -37,6 +36,7 @@ public class DownloadTest {
 		}
 		outputStream.flush();
 		outputStream.close();
+		bufferedIn.close();
 	}
 	
 }
